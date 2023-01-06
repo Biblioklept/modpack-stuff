ef53241ffe31b35c86b4ccd620f8ed8125652eba
Biblioklept <61851290+Biblioklept@users.noreply.github.com>
2023-01-06 12:54:39 +0000

    Update install.bat
    fixed install.bat delete line, script is now fully automated.

--- oblivion/install.bat
+++ oblivion/install.bat
@@ -3,9 +3,10 @@ mkdir OBL
 cd OBL
 curl https://www.7-zip.org/a/7zr.exe -L -o 7zr.exe
 curl https://github.com/ModOrganizer2/modorganizer/releases/download/v2.4.4/Mod.Organizer-2.4.4.7z -L -o MO2.7z
-curl https://github.com/Biblioklept/modpack-stuff/releases/download/dl/OBL.exe -L -o OBL.exe
+curl https://github.com/Biblioklept/modpack-stuff/releases/download/dl/OBL.7z -L -o OBL.7z
 7zr x MO2.7z
+7zr x OBL.7z -p zf
 del MO2.7z
 del 7zr.exe
-start obl.exe
+cd ..
 del install.bat
