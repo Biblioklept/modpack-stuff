fcdd6992630d2d90531b5d6118d44d0dd2791721
Biblioklept <61851290+Biblioklept@users.noreply.github.com>
2023-01-06 13:00:44 +0000

    Update install.bat

--- oblivion/install.bat
+++ oblivion/install.bat
@@ -5,7 +5,7 @@ curl https://www.7-zip.org/a/7zr.exe -L -o 7zr.exe
 curl https://github.com/ModOrganizer2/modorganizer/releases/download/v2.4.4/Mod.Organizer-2.4.4.7z -L -o MO2.7z
 curl https://github.com/Biblioklept/modpack-stuff/releases/download/dl/OBL.7z -L -o OBL.7z
 7zr x MO2.7z
-7zr x OBL.7z -p zf
+7zr x OBL.7z -p"zf"
 del MO2.7z
 del 7zr.exe
 cd ..
